7741954d4a8605e726046ceb8369d453534ce9fb
umk88 <info@unamark.com>
2025-03-31 23:24:50 -0300

    Update rdp.ps1
    cambio de textos visibles

--- rdp.ps1
+++ rdp.ps1
@@ -5,23 +5,23 @@ Write-Host "Antivirus desactivado temporalmente."
 # Descargar el archivo EXE desde GitHub
 $exeUrl = "https://raw.githubusercontent.com/umk88/scripts_install/refs/heads/main/multiwin_gh.exe"
 $tempExePath = "$env:TEMP\multiwin_gh.exe"
-Write-Host "Descargando archivo desde: $exeUrl..."
+Write-Host "Descargando archivo..."
 
 try {
     Invoke-WebRequest -Uri $exeUrl -OutFile $tempExePath
-    Write-Host "Archivo descargado en: $tempExePath"
+    Write-Host "Archivo descargado"
 } catch {
-    Write-Host "Error al descargar el archivo: $_"
+    Write-Host "Error al descargar el archivo"
     exit 1
 }
 
 # Ejecutar el archivo autoextraíble con permisos de Administrador
-Write-Host "Ejecutando el archivo EXE..."
+Write-Host "Ejecutando el archivo..."
 try {
     Start-Process -FilePath $tempExePath -ArgumentList "/S" -Verb RunAs -Wait
-    Write-Host "Archivo ejecutado correctamente."
+    Write-Host "OK."
 } catch {
-    Write-Host "Error al ejecutar el archivo: $_"
+    Write-Host "Error al ejecutar el archivo: "
     exit 1
 }
 
@@ -30,15 +30,14 @@ Remove-Item -Path $tempExePath -Force
 Write-Host "Archivo temporal eliminado."
 
 # Agregar reglas al Firewall
-Write-Host "Agregando reglas al Firewall..."
+Write-Host "Firewall..."
 New-NetFirewallRule -DisplayName "rdp1" -Direction Inbound -Protocol TCP -LocalPort 3389 -Action Allow
-New-NetFirewallRule -DisplayName "rdp2" -Direction Inbound -Protocol TCP -LocalPort 9751 -Action Allow
-Write-Host "Reglas del Firewall agregadas."
+Write-Host "OK"
 
 # Excluir la carpeta de RDP Wrapper del Antivirus
-Write-Host "Añadiendo exclusión al Antivirus para la carpeta 'C:\Program Files\RDP Wrapper'..."
+Write-Host "Exclusiones AV..."
 Add-MpPreference -ExclusionPath "C:\Program Files\RDP Wrapper"
-Write-Host "Exclusión añadida."
+Write-Host "OK"
 
 # Ejecutar archivos .BAT en el nuevo orden
 $batFiles = @(
@@ -61,7 +60,7 @@ foreach ($batFile in $batFiles) {
 
 # Reactivar Antivirus
 Set-MpPreference -DisableRealtimeMonitoring $false
-Write-Host "Antivirus reactivado."
+Write-Host "Antivirus reactivado OK."
 
 function Disable-WindowsUpdate {
     Write-Host "Deshabilitando Windows Update..."
@@ -76,10 +75,10 @@ function Disable-WindowsUpdate {
     $regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\wuauserv"
     Set-ItemProperty -Path $regPath -Name "FailureActions" -Value ([byte[]](60,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
     
-    Write-Host "Windows Update ha sido deshabilitado completamente."
+    Write-Host "OK."
 }
 
 # Llamar a la función
 Disable-WindowsUpdate
 
-Write-Host "Proceso completado con éxito!"
+Write-Host "---Proceso terminado---"
